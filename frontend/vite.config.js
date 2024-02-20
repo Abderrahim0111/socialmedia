43a931ce4fde7ff72df649fe8638b24b84f9c743
Abderrahim0111 <abderrahi.defaoui@gmail.com>
2024-02-20 18:20:38 +0100

    final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: `${import.meta.env.VITE_ENDPOINT}`,
-        secure: false,
+export default defineConfig(({ command, mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_ENDPOINT,
+          secure: false,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
