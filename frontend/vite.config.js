9aaf85719def46874a69257a114539f984dfb876
Abderrahim0111 <abderrahi.defaoui@gmail.com>
2024-02-26 18:04:27 +0100

    final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,16 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_ENDPOINT,
-          secure: false,
-        },
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://socialsnap.onrender.com",
+        changeOrigin: true,
+        secure: false,
       },
     },
-    plugins: [react()],
-  };
+  },
+  plugins: [react()],
 });
